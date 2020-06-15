65cc07b48c87d19bc3504b38af37983f75588435
Humberto Tello <betoelo2@gmail.com>
2020-06-15 16:49:49 -0300

    Escopos

--- Introdução à programação web/Aulas/loopFor.js
+++ Introdução à programação web/Aulas/loopFor.js
@@ -17,7 +17,7 @@ const alunosDaTurmaA = [
   }
 ]
 
-const alunosDaTurmaB = [
+const alunosDaTurmaB = [ // const não permite que mude o tipo de variável, mas permite mudança de valor dentro dela
   {
     nome: 'Cleiton',
     nota: 10
@@ -39,12 +39,11 @@ const alunosDaTurmaB = [
 function calculaMedia(alunos) { // alunos sera o objeto
   let soma = 0
   for (let i = 0; i < alunos.length; i++) { // let permite mudar a variável // .length conta o número de objetos no array // i++ incremento
-    soma = soma + alunos[i].nota
+    soma = soma + alunos[i].nota            // o i só está definido dentro do loop for, só pode ser chamado nesse contexto
   }
-
   const media = soma / alunos.length
   console.log(media)
-  return media
+  return media // precisa retornar, pois media so existe dentro do escopo de bloco da função
 }
 
 const media1 = calculaMedia(alunosDaTurmaA) // o parâmetro é um objeto
@@ -60,3 +59,5 @@ function enviaMensagem(media, turma) { // media = number e turma = string
 
 enviaMensagem(media1, 'turmaA')
 enviaMensagem(media2, 'turmaB')
+
+let alunos = ['Fulano', 'Siclano'] // tem escopo global, pode ser chamada e modificada dentro de uma função
