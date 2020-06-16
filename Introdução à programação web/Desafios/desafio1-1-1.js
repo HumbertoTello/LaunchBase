6a5b36ca8bec071f5f9a44207129fbfb976ba8a5
Humberto Tello <betoelo2@gmail.com>
2020-06-15 23:06:01 -0300

    Desafio 1-3

---
+++ Introdução à programação web/Desafios/desafio1-1-1.js
@@ -0,0 +1,13 @@
+const nome = 'Humberto'
+const peso = 50
+const altura = 1.70
+
+const imc = peso / (altura * altura)
+
+if (imc >= 30) {
+  console.log(`${nome} você está acima do peso`)
+} 
+
+if (imc < 29.9) {
+  console.log(`${nome} você não está acima do peso`)
+}
