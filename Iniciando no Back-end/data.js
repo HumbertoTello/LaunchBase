6b9d0102c2e9b1c1b5efe284badeb5f051b7461d
Humberto Tello <betoelo2@gmail.com>
2020-06-18 20:29:09 -0300

    Dados dinâmicos

---
+++ Iniciando no Back-end/data.js
@@ -0,0 +1,39 @@
+module.exports = [
+  {
+    id: "2alg7MQ6_sI",
+    title: "Git e Github na prática",
+    duration: "33 min",
+    price: "Free"
+  },
+  {
+    id: "x4FdZd2-_uU",
+    title: "Recriando a interface do Discord com ReactJS",
+    duration: "2 h 22 min",
+    price: "Free"
+  },
+  {
+    id: "GTMEuHxh8aQ",
+    title: "Validação de forms customizada com HTML e JavaScript",
+    duration: "1 h 31 min",
+    price: "Free"
+  },
+  {
+    id: "EhnXaybirdA",
+    title: "Criando um projeto do zero para seu portfólio",
+    duration: "44 min",
+    price: "Free"
+  },
+  {
+    id: "u99tNt3TZf8",
+    title: "Desvendando Redux na Prática",
+    duration: "50 min",
+    price: "Free"
+  },
+  {
+    id: "HN1UjzRSdBk",
+    title: "Desvendando o CSS Grid na prática",
+    duration: "36 min",
+    price: "Free"
+  },
+  
+]
--- Iniciando no Back-end/server.js
+++ Iniciando no Back-end/server.js
@@ -2,23 +2,37 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
+const videos = require("./data")
 
 server.use(express.static('public'))
 
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-  express: server
+  express: server,
+  autoescape: false
 })
 
 server.get("/", function (req, res) {
-  return res.render("about")
+  const about = {
+    avatar_url: "https://avatars2.githubusercontent.com/u/6643122?s=400&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
+    name: "Mayk Brito",
+    role: "Instrutor - Rocketseat",
+    description: 'Programador full-stack focado em trazer o melhor ensino para iniciantes em programação. Colaboradora da <a href="https://rocketseat.com.br" target="_blank">Rocketseat</a>',
+    links: [
+      { name: "Github", url: "https://github.com/maykbrito"},
+      { name: "Twitter", url: "https://twitter.com/maykbrito"},
+      { name: "Linkedin", url: "https://www.linkedin.com/in/maykbrito/"}
+    ]
+  }
+
+  return res.render("about", { about })
 })
 
 server.get("/portfolio", function (req, res) {
-  return res.render("portfolio")
+  return res.render("portfolio", { items: videos })
 })
 
-server.listen("5000", function () {
+server.listen("5001", function () {
   console.log("Server is running")
 })
