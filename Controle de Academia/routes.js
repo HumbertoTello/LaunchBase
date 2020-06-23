f022322e0542f4882e4504e5aff7a270a7ffb8ad
Humberto Tello <betoelo2@gmail.com>
2020-06-23 15:09:56 -0300

    Data validation

--- Controle de Academia/routes.js
+++ Controle de Academia/routes.js
@@ -9,6 +9,30 @@ routes.get('/instructors', function(req,res) {
   return res.render("instructors/index")
 })
 
+routes.get('/instructors/create', function(req, res) {
+  return res.render('instructors/create')
+})
+
+routes.post("/instructors", function(req,res) {
+  const keys = Object.keys(req.body)
+
+  //req.body
+  //{"avatar_url":"http://google.com","name":"Jéssica","birth":"1994-02-12","gender":"F","services":"Ciclismo"}
+  
+  //req.keys
+  //["avatar_url","name","birth","gender","services"]
+
+  for (key of keys) {
+
+    // req.body.key == ""
+    if (req.body[key] == "") {
+      return res.send('Please, fill all fields')
+    }
+  }
+  
+  return res.send(req.body)
+})
+
 routes.get('/members', function(req,res) {
   return res.send("members")
 })
