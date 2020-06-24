50a789f3865cb318227b93f7a7c137da2a4f446a
Humberto Tello <betoelo2@gmail.com>
2020-06-24 09:00:36 -0300

    Challenge 4-3

--- Controle de Academia/challenges/routes.js
+++ Controle de Academia/challenges/routes.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const routes = express.Router()
+const teachers = require('./teachers')
 
 routes.get('/', function(req,res) {
-  return res.render("index")
+  return res.redirect("/teachers")
 })
+
+routes.get("/teachers", function(req, res) {
+  return res.render("teachers/index")
+})
+
+routes.get('/teachers/create', function(req, res) {
+  return res.render("teachers/forms")
+})
+
+routes.post('/teachers', teachers.post)
 
 module.exports = routes
