b88efddf5f740c9d422636937dd512e630de28ff
Humberto Tello <betoelo2@gmail.com>
2020-06-25 13:14:53 -0300

    Challenge 4-4

--- Controle de Academia/challenges/routes.js
+++ Controle de Academia/challenges/routes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const routes = express.Router()
 const teachers = require('./teachers')
+const { get } = require('browser-sync')
 
 routes.get('/', function(req,res) {
   return res.redirect("/teachers")
@@ -13,6 +14,10 @@ routes.get("/teachers", function(req, res) {
 routes.get('/teachers/create', function(req, res) {
   return res.render("teachers/forms")
 })
+
+routes.get('/teachers/:id', teachers.show)
+
+routes.get('/teachers/:id/edit', teachers.edit)
 
 routes.post('/teachers', teachers.post)
 
--- Controle de Academia/challenges/teachers.js
+++ Controle de Academia/challenges/teachers.js
@@ -1,5 +1,7 @@
 const fs = require('fs')
 const data = require('./data.json')
+const { timeStamp } = require('console')
+const { age, date } = require('./utils')
 
 exports.post = function(req, res) {
   const keys = Object.keys(req.body)
@@ -13,6 +15,7 @@ exports.post = function(req, res) {
   let {avatar_url, name, birth, degree, classType, subject} = req.body
 
   birth = Date.parse(birth)
+  const created_at = Date.now()
   const id = Number(data.teachers.length + 1)
 
   data.teachers.push({
@@ -22,7 +25,8 @@ exports.post = function(req, res) {
     birth,
     degree,
     classType,
-    subject
+    subject,
+    created_at
   })
 
   fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
@@ -30,4 +34,41 @@ exports.post = function(req, res) {
 
     return res.redirect('/teachers')
   })
+}
+
+exports.show = function(req, res) {
+  const { id } = req.params
+  const foundTeacher = data.teachers.find(function(teacher) {
+    return teacher.id == id
+  })
+
+  if (!foundTeacher) {
+    return res.send("Teacher not found")
+  }
+
+  const teacher = {
+    ...foundTeacher,
+    subject: foundTeacher.subject.split(','),
+    created_at: new Intl.DateTimeFormat("pt-BR").format(foundTeacher.created_at),
+    birth: age(foundTeacher.birth)
+  }
+
+  return res.render("teachers/show", { teacher })
+}
+
+exports.edit = function(req, res) {
+  const { id } = req.params
+
+  const foundTeacher = data.teachers.find(function(teacher) {
+    return teacher.id == id
+  })
+
+  if (!foundTeacher) return res.send("Teacher not found")
+
+  const teacher = {
+    ...foundTeacher,
+    birth: date(foundTeacher.birth)
+  }
+
+  return res.render('teachers/edit', { teacher })
 }
