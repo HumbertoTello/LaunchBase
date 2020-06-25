66b4f963fc34e39ba1918caa1ffe2a78f84bdad7
Humberto Tello <betoelo2@gmail.com>
2020-06-24 22:27:04 -0300

    Managing data

--- Controle de Academia/instructors.js
+++ Controle de Academia/instructors.js
@@ -10,7 +10,15 @@ exports.show = function (req, res) {
 
   if (!foundInstructor) return res.send("Instructor not found")
 
-  return res.send(foundInstructor)
+  const instructor = {
+    ...foundInstructor,
+    age: "",
+    gender: "",
+    services: foundInstructor.services.split(","),
+    created_at: ""
+  }
+
+  return res.render("instructors/show", { instructor })
 }
 
 // create
