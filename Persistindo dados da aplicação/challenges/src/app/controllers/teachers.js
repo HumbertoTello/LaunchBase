49b697cc2f47c30f1de355f274fe0e6b56e64577
Humberto Tello <betoelo2@gmail.com>
2020-06-29 13:22:27 -0300

    Challenge 5-1

---
+++ Persistindo dados da aplicação/challenges/src/app/controllers/teachers.js
@@ -0,0 +1,43 @@
+const { age, date } = require('../../lib/utils')
+
+module.exports = {
+  index(req, res){
+    return res.render("teachers/index")
+  },
+  create(req, res){
+    return res.render("teachers/forms")
+  },
+  post(req, res){
+    const keys = Object.keys(req.body)
+
+    for (key of keys) {
+      if (req.body[key] == "") {
+        return res.send("Please, fill all fields")
+      }
+    }
+  
+    let {avatar_url, name, birth, degree, classType, subject} = req.body
+
+    return
+  },
+  show(req, res){
+    return
+  },
+  edit(req, res){
+    return
+  },
+  put(req, res){
+    const keys = Object.keys(req.body)
+
+    for (key of keys) {
+      if (req.body[key] == "") {
+        return res.send("Please, fill all fields")
+      }
+    }
+
+    return
+  },
+  delete(req, res){
+    return
+  }
+}
