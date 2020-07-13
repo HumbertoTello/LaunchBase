5b3e1364a82adc08f56a2d937620a2bdb5788525
Humberto Tello <betoelo2@gmail.com>
2020-07-13 20:10:21 -0300

    Product form

--- LaunchStore/src/routes.js
+++ LaunchStore/src/routes.js
@@ -6,9 +6,16 @@
 
 const express = require('express')
 const routes = express.Router()
+const ProductControler = require('./app/controllers/ProductController')
 
 routes.get('/', function(req,res) {
   return res.render("layout.njk")
+})
+
+routes.get('/products/create', ProductControler.create)
+
+routes.get('ads/create', function(req,res) {
+  return res.redirect("/products/create")
 })
 
 module.exports = routes
