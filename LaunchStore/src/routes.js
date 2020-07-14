bbf825e46e0a7168edd02559708bd039d8d24c59
Humberto Tello <betoelo2@gmail.com>
2020-07-14 10:50:06 -0300

    Products create

--- LaunchStore/src/routes.js
+++ LaunchStore/src/routes.js
@@ -1,19 +1,15 @@
-// HTTP
-// GET: Receive RESOURCE (entity)
-// POST: Create new RESOURCE with sent data
-// PUT: Update RESOURCE
-// DELETE: Delete RESOURCE
-
 const express = require('express')
 const routes = express.Router()
-const ProductControler = require('./app/controllers/ProductController')
+const ProductController = require('./app/controllers/ProductController')
 
 routes.get('/', function(req,res) {
   return res.render("layout.njk")
 })
 
-routes.get('/products/create', ProductControler.create)
+routes.get('/products/create', ProductController.create)
+routes.post('/products', ProductController.post)
 
+// Alias
 routes.get('ads/create', function(req,res) {
   return res.redirect("/products/create")
 })
