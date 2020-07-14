933904760ee2cac27eea045567d359f6eb139e54
Humberto Tello <betoelo2@gmail.com>
2020-07-14 14:57:57 -0300

    Edit, update and delete

--- LaunchStore/src/routes.js
+++ LaunchStore/src/routes.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const routes = express.Router()
 const ProductController = require('./app/controllers/ProductController')
+const Product = require('./app/models/Product')
 
 routes.get('/', function(req,res) {
   return res.render("layout.njk")
 })
 
 routes.get('/products/create', ProductController.create)
+routes.get('/products/:id/edit', ProductController.edit)
+
 routes.post('/products', ProductController.post)
+routes.put('/products', ProductController.put)
+routes.delete('/products', ProductController.delete)
 
 // Alias
 routes.get('ads/create', function(req,res) {
