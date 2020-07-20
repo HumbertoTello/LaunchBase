27efd3b010d9de35e88a8c9e01d97c35c0f3a928
Humberto Tello <betoelo2@gmail.com>
2020-07-20 12:03:49 -0300

    Refactoring code

--- LaunchStore/public/scripts.js
+++ LaunchStore/public/scripts.js
@@ -15,16 +15,12 @@ const Mask = {
 }
 
 const PhotosUpload = {
+  preview: document.querySelector("#photos-preview"),
   uploadLimit: 6,
   handleFileInput(event) {
     const { files: fileList } = event.target
-    const { uploadLimit } = PhotosUpload
 
-    if (fileList.length > uploadLimit) {
-      alert(`Envie no máximo ${uploadLimit} arquivos`)
-      event.preventDefault()
-      return
-    }
+    if (PhotosUpload.hasLimit(event)) return
 
     Array.from(fileList).forEach (file => {
       const reader = new FileReader()
@@ -33,17 +29,32 @@ const PhotosUpload = {
         const image = new Image()
         image.src = String(reader.result)
 
-        const container = document.createElement("div")
-        container.classList.add("photo")
-
-        container.onclick = () => alert("Remover foto")
-        
-        container.appendChild(image)
-
-        document.querySelector("#photos-preview").appendChild(container)
+        const div = PhotosUpload.getContainer(image)
+        PhotosUpload.preview.appendChild(div)
       }
 
       reader.readAsDataURL(file)
     })
+  },
+  hasLimit(event) {
+    const { uploadLimit } = PhotosUpload
+
+    if (fileList.length > uploadLimit) {
+      alert(`Envie no máximo ${uploadLimit} arquivos`)
+      event.preventDefault()
+      return true
+    }
+
+    return false
+  },
+  getContainer(image) {
+    const div = document.createElement("div")
+    div.classList.add("photo")
+
+    div.onclick = () => alert("Remover foto")
+    
+    div.appendChild(image)
+
+    return div
   }
 }
