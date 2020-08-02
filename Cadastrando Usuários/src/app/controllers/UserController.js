43a6c26fad49a3d641d983c066c92edc994993da
Humberto Tello <betoelo2@gmail.com>
2020-08-02 17:10:15 -0300

    Finding user

--- Cadastrando Usuários/src/app/controllers/UserController.js
+++ Cadastrando Usuários/src/app/controllers/UserController.js
@@ -1,6 +1,29 @@
+const User = require('../models/User')
+
 module.exports = {
   registerForm(req, res) {
     return res.render("user/register")
+  },
+  post(req, res) {
+    //check if has all fields
+    const keys = Object.keys(req.body)
+
+    for (key of keys) {
+      if(req.body[key] == "") {
+        return res.send("Please, fill all fields")
+      }
+    }
+
+    //check if user exists [email, cpf_cnpj]
+    const { email, cnpj } = req.body
+    const user = await User.findOne({
+      where: {email},
+      or: {cpf_cnpj}
+    })
+
+    //check if password match
+
+
   }
 }
 // class Person {
