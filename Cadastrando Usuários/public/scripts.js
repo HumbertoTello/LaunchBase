efa0b0efa23b9ea693b63b2598266bbc97c11938
Humberto Tello <betoelo2@gmail.com>
2020-08-02 14:28:07 -0300

    CEP, CPF, CNPJ Mask

--- Cadastrando Usuários/public/scripts.js
+++ Cadastrando Usuários/public/scripts.js
@@ -11,6 +11,46 @@ const Mask = {
       style: "currency",
       currency: "BRL"
     }).format(value/100)
+  },
+  cpfCnpj(value) {
+    value = value.replace(/\D/g, "")
+
+    if(value.length > 11) {
+      value = value.slice(0, -1)
+    }
+    // check if is cpf or cnpj - 11.222.333/0001-11
+    if(value.length > 11) {
+      //11222333444455
+
+      //11.222333444455
+      value = value.replace(/(\d{2})(\d)/, "$1.$2")
+
+      //11.222.333444455
+      value = value.replace(/(\d{3})(\d)/, "$1.$2")
+
+      //11.222.333/444455
+      value = value.replace(/(\d{3})(\d)/, "$1/$2")
+
+      //11.222.333444455
+      value = value.replace(/(\d{4})(\d)/, "$1-$2")
+    } else {
+      value = value.replace(/(\d{3})(\d)/, "$1.$2")
+      value = value.replace(/(\d{3})(\d)/, "$1.$2")
+      value = value.replace(/(\d{3})(\d)/, "$1-$2")
+    }
+
+    return value
+  },
+  cep(value) {
+    value = value.replace(/\D/g, "")
+
+    if(value.length > 8) {
+      value = value.slice(0, -1)
+    }
+
+    value = value.replace(/(\d{5})(\d)/, "$1-$2")
+
+    return value
   }
 }
 
