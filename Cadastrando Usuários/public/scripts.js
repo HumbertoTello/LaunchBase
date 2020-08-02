604917fc3230bf1fa05dd71f5aae4eaeb0f10187
Humberto Tello <betoelo2@gmail.com>
2020-08-02 15:48:07 -0300

    Email validation

--- Cadastrando Usuários/public/scripts.js
+++ Cadastrando Usuários/public/scripts.js
@@ -15,7 +15,7 @@ const Mask = {
   cpfCnpj(value) {
     value = value.replace(/\D/g, "")
 
-    if(value.length > 11) {
+    if(value.length > 14) {
       value = value.slice(0, -1)
     }
     // check if is cpf or cnpj - 11.222.333/0001-11
@@ -187,5 +187,38 @@ const Lightbox = {
     Lightbox.target.style.top = "-100%"
     Lightbox.target.style.bottom = "initial"
     Lightbox.closeButton.style.top = "-80px"
+  }
+}
+
+const Validate = {
+  apply(input, func) {
+    Validate.clearErrors(input)
+    let results = Validate[func](input.value)
+    input.value = results.value
+
+    if (results.error)
+      Validate.displayError(input, results.error)
+  },
+  displayError(input, error) {
+    const div = document.createElement('div')
+    div.classList.add('error')
+    div.innerHTML = error
+    input.parentNode.appendChild(div)
+    input.focus()
+  },
+  clearErrors(input) {
+    const errorDiv = input.parentNode.querySelector(".error")
+    if (errorDiv)
+      errorDiv.remove()
+  },
+  isEmail(value) {
+    let error = null
+    const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    if (!value.match(mailFormat))
+      error = "Email inválido"
+    return {
+      error,
+      value
+    }
   }
 }
