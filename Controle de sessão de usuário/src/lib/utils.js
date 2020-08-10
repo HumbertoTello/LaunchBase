f02e5e8061e2b8a49f541b3e591d559056f67a11
Humberto Tello <betoelo2@gmail.com>
2020-08-10 09:45:08 -0300

    First commit

---
+++ Controle de sessão de usuário/src/lib/utils.js
@@ -0,0 +1,35 @@
+module.exports = {
+  date(timestamp) {
+    const date = new Date(timestamp)
+
+    // yyyy
+    const year = date.getFullYear() // UTC == universal time
+
+    // mm (0 to 11)
+    const month = `0${date.getMonth() + 1}`.slice(-2)
+
+    // dd (1 to 31)
+    const day = `0${date.getDate()}`.slice(-2)
+
+    const hour = date.getHours()
+    const minutes = date.getMinutes()
+
+    return {
+      day,
+      month,
+      year,
+      hour,
+      minutes,
+      iso: `${year}-${month}-${day}`,
+      birthDay: `${day}/${month}`,
+      format: `${day}/${month}/${year}`
+    } 
+  },
+  formatPrice(price) {
+    return new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL"
+    }).format(price/100)
+  }
+}
+
---
+++ Controle de sessão de usuário/src/routes/users.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const routes = express.Router()
+
+const SessionController = require('../app/controllers/SessionController')
+const UserController = require('../app/controllers/UserController')
+
+const Validator = require('../app/validators/user')
+
+// // login / logout
+// routes.get('/users/login', SessionController.loginForm)
+// routes.post('/users/login', SessionController.login)
+// routes.post('/users/logout', SessionController.logout)
+// // reset password / forgot
+// routes.get('/users/forgot-password', SessionController.forgotForm)
+// routes.get('/users/password-reset', SessionController.resetForm)
+// routes.post('/users/forgot-password', SessionController.forgot)
+// routes.post('/users/password-reset', SessionController.reset)
+
+// // user register UserController
+routes.get('/register', UserController.registerForm)
+routes.post('/register', Validator.post, UserController.post)
+routes.get('/', UserController.show)
+// routes.put('/', UserController.update)
+// routes.delete('/', UserController.delete)
+
+module.exports = routes
