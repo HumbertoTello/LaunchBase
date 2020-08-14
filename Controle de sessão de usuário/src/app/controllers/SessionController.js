592d465d436b164f35e98d36d96005fb2a8f8781
Humberto Tello <betoelo2@gmail.com>
2020-08-14 08:33:59 -0300

    Login logic

--- Controle de sessão de usuário/src/app/controllers/SessionController.js
+++ Controle de sessão de usuário/src/app/controllers/SessionController.js
@@ -1,6 +1,15 @@
 module.exports = {
   loginForm(req, res) {
-    return res.render("session/index")
+    return res.render("session/login")
+  },
+  login(req, res) {
+    // verificar se o usuário está cadastrado
+    req.session.userId = req.user.id
+
+    return res.redirect("/users")
+    // verificar se o password bate
+
+    // colocar o usuário no req.session (deixar logado)
   },
   logout(req, res) {
     req.session.destroy()
