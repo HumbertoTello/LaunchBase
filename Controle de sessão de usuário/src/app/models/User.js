c0ff343496c5695952d42b6718df976a2f0d6efe
Humberto Tello <betoelo2@gmail.com>
2020-08-17 08:34:00 -0300

    SQL Cascade

--- Controle de sessão de usuário/src/app/models/User.js
+++ Controle de sessão de usuário/src/app/models/User.js
@@ -1,6 +1,7 @@
 const db = require('../../config/db')
 const { hash } = require('bcryptjs')
-
+const fs = require('fs')
+const Product = require('../models/Product')
 
 module.exports = {
   async findOne(filters) {
@@ -71,6 +72,25 @@ module.exports = {
 
     await db.query(query)
     return
+  },
+  async delete(id) {
+    // pegar todos os produtos do user_id
+    let results = await db.query("SELECT * FROM products WHERE user_id = $1", [id])
+    const products = results.rows
+
+    // dos produtos, pegar todas as imagens
+    const allFilesPromise = products.map(product => 
+      Product.files(product.id))
+
+    let promiseResults = await Promise.all(allFilesPromise)
+
+    // rodar a remoção do usuário
+    await db.query('DELETE FROM users WHERE id = $1', [id])
+
+    // remover as imagens da pasta public
+    promiseResults.map(results => {
+      results.rows.map(file => fs.unlinkSync(file.path))
+    })
   }
 }
 
